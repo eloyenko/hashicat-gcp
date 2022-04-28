10cafc6d08bb3b9ff64a346b7fe63f40068a4ced
eloyenko <104505150+eloyenko@users.noreply.github.com>
2022-04-28 17:10:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,11 +48,6 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
   
-  labels = {
-    department   = "devops"
-    billable     = true
-  }
-  
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
@@ -73,6 +68,8 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department = "devops"
+    billable = true
   }
 
 }
